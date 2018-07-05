ed3b004ecdd6c8b3b85c6c80ff62880cbc6c53e7
Kelvin Ashu <kelvin@pinevalleyinstitute.co.za>
2018-07-05 23:59:44 +0200

    WhatsApp flutter

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,57 @@
+class ChatModel{
+  final String name;
+  final String message;
+  final String time;
+  final String avatarURL;
+
+  ChatModel({this.name, this.message, this.time, this.avatarURL});
+
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+    name: "Grace S Lindberg",
+    message: "I am on the way",
+    time: "10:23",
+    avatarURL: "https://www.fakepersongenerator.com/Face/female/female20081023287249390.jpg"
+  ),
+  new ChatModel(
+    name: "George L Drake",
+    message: "Hello world!",
+    time: "30:43",
+    avatarURL: "https://www.fakepersongenerator.com/Face/female/female20161025040268646.jpg"
+  ),
+  new ChatModel(
+    name: "John R Woodward",
+    message: "See attached ...",
+    time: "09:12",
+    avatarURL: "https://www.fakepersongenerator.com/Face/female/female20171026146744568.jpg"
+  ),
+  new ChatModel(
+    name: "Miguel",
+    message: "Its sold already",
+    time: "11:12",
+    avatarURL: "https://www.fakepersongenerator.com/Face/female/female20131023651640900.jpg"
+  ),
+  new ChatModel(
+    name: "Larson",
+    message: "Where did you leave it?",
+    time: "07:00",
+    avatarURL: "https://www.fakepersongenerator.com/Face/male/male108477720661.jpg"
+  ),new ChatModel(
+    name: "Florence",
+    message: "lol",
+    time: "14:19",
+    avatarURL: "https://www.fakepersongenerator.com/Face/female/female102200444220.jpg"
+  ),new ChatModel(
+    name: "Arterburn",
+    message: "Confirm....",
+    time: "10:23",
+    avatarURL: "https://www.fakepersongenerator.com/Face/female/female1023120355194.jpg"
+  ),new ChatModel(
+    name: "Norma S Winborne",
+    message: "Ok will be there in a sec.",
+    time: "14:09",
+    avatarURL: "https://www.fakepersongenerator.com/Face/male/male1084707780853.jpg"
+  )
+];
